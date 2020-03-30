0cacbf822da1932d555570add24898e12c1298e9
Riko gusjakal muran <54266098+Riko164@users.noreply.github.com>
2020-03-30 20:35:01 +0700

    Create little_duck.py

---
+++ uts/little_duck.py
@@ -0,0 +1,9 @@
+def little_duck(n):
+    for i in range(n,0,-1):
+        if i==1:
+            print(i, "little duck went out one day, 1 was missing")
+        else:
+            print(i, "little ducks went out one day, 1 was missing")
+print("all ducks were missing")
+
+little_duck(3)
